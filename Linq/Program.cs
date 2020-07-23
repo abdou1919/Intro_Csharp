0476f623f0e4c67adbb190a6688dcd2a1608e81f
Ben Abdesslem <abdou1919@hotmail.com>
2020-07-23 14:13:23 +0200

    Importation_Projet_C#

---
+++ Linq/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>()
+
+            {
+"Michael",
+"Christopher",
+"Jessica",
+"Matthew",
+"Ashley",
+"Jennifer",
+"Joshua",
+"Amanda",
+"Daniel",
+"David",
+"James",
+"Robert",
+"John",
+"Joseph",
+"Andrew",
+"Ryan",
+"Brandon",
+"Jason",
+"Justin",
+"Sarah",
+"William",
+"Jonathan",
+"Stephanie",
+"Brian"
+,"Nicole"
+,"Nicholas"
+,"Anthony"
+,"Heather",
+"Eric",
+"Elizabeth",
+
+            };
+
+
+            //var r = from x in Moyennes where x >= 10 select x;
+
+
+
+            //var r = Moyennes.Where(item => item > 10);
+
+            var r = names.Where(x => (x.Length > 5 && x.StartsWith("A")) || x.EndsWith("n"));
+
+
+
+            int i = 1;
+
+            foreach (var item in r)
+            {
+                Console.WriteLine($"{i}------{item}  ");
+                i++;
+
+            }
+
+            Console.WriteLine($"______________________________");
+            Console.WriteLine($" Tolal {i - 1}");
+
+            Console.Read();
+
+        }
+
+
+
+    }
+}
---
+++ Linq_Class/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+
+namespace Linq_Class
+{
+    class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book[] books = {
+
+            new Book() {Id=6,Title="Zitouna", Author="Slim" },
+            new Book() {Id=6,Title="Filej", Author="Abdesslem" },
+            new Book() {Id=3,Title="Ghilen",Author="Badra" },
+            new Book() {Id=2,Title="Ghilta",Author="Nabil" },
+            new Book() {Id=1,Title="Eliemaa",Author="Saber" },
+            new Book() {Id=8,Title="Rome", Author="Abdesslem" }
+
+            };
+
+
+            //var List = from item 
+            //           in books
+            //           where item.Title=="zitouna"
+            //           select item;
+
+
+            //var List = books.Where(item=> item.Title=="Zitouna");
+
+            //------------------------------------------------------------------------------------------------------
+            //var List1 = books.OrderBy(item => item.Id).ThenBy(item=> item.Author);
+            //------------------------------------------------------------------------------------------------------
+
+            var List = books.GroupBy(item => item.Author);
+
+
+            foreach (var item in List)
+            {
+                Console.WriteLine("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+                Console.WriteLine($"Author: {item.Key}");
+
+
+                foreach (var auth_book in item)
+                {
+
+
+                    Console.WriteLine($"\n {auth_book.Title}");
+
+
+
+                }
+                //Console.WriteLine($"{item.Id} {item.Author} {item.Title}");
+                //Console.WriteLine("-----------------------------------------------");
+            }
+
+            Console.Read();
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
