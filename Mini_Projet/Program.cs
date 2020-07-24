ed479907bc214d3098964b8d6ea4a270b8071e71
Ben Abdesslem <abdou1919@hotmail.com>
2020-07-24 11:45:05 +0200

    Mini_Projet

---
+++ Mini_Projet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mini_Projet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Mini_Projet_CSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_Projet_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
